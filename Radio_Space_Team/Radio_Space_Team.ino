b9bf8ca2bcff79a33039d21bd4ab602cfc6eef53
Jeffrey Jiang <jiangxuanzhi@gmail.com>
2016-11-03 12:03:13 -0700

    it's not working. :( at least it's compiling.

--- Radio_Space_Team/Radio_Space_Team.ino
+++ Radio_Space_Team/Radio_Space_Team.ino
@@ -1,19 +1,31 @@
-void radio_init() {
-  setPALevel(RF_PA_LOW);
-  setPayloadSize(32);
-  setChannel(7);
-  setCRCLength(RF24_CRC_16);
-  setDataRate(RF24_1MPS);
-  openReadingPipe(0xCC);
-  openWritingPipe(0xDC);
-}
+#include <printf.h>
+#include <RF24.h>
+//#include <nrf24l01p.h>
+
+//void radio_init() {
+//  setPALevel(RF24_PA_LOW);
+//  setPayloadSize(32);
+//  setChannel(7);
+//  setCRCLength(RF24_CRC_16);
+//  setDataRate(RF24_1MPS);
+//  openReadingPipe(0xCC);
+//  openWritingPipe(0xDC);
+//}
+
+RF24 controller(23, 18);
 void setup() {
+  Serial.begin(115200);
+  printf_begin();
+  //RF24 controller(23, 18);
   // put your setup code here, to run once:
-setPALevel(RF_PA_LOW);
+  controller.setPALevel(RF24_PA_LOW);
+  controller.printDetails();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  controller.printDetails();
+  delay(500);
 
 }
