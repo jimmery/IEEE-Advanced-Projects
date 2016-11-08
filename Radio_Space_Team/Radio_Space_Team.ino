b3f543b44abf6a3cb648c2f2bb2e29f6886c2596
shawnnanigans <shawnlo.me@gmail.com>
2016-11-08 10:35:34 -0800

    removed extra arduino file
    may be cause of problem

--- Radio_Space_Team/Radio_Space_Team.ino
+++ Radio_Space_Team/Radio_Space_Team.ino
@@ -12,29 +12,33 @@
 #define CEpin 14
 #define CSpin 15
 
+
+//
 //void radio_init() {
 //  setPALevel(RF24_PA_LOW);
 //  setPayloadSize(32);
-//  setChannel(7);
+//  setChannel(7); //team number
 //  setCRCLength(RF24_CRC_16);
 //  setDataRate(RF24_1MPS);
-//  openReadingPipe(0xCC);
-//  openWritingPipe(0xDC);
+//  openReadingPipe(1, 0xDC); //Writing pipe of the Teensy
+//  openWritingPipe(0xCC); //Reading pipe of the Teensy
 //}
 
-RF24* controller;
 
+
+
+  
 void setup() {
-  controller = new RF24(CEpin, CSpin);
+
   Serial.begin(9600);
-  controller->begin();
+RF24 controller(CEpin, CSpin);
+  controller.begin();
   printf_begin();
-  //put your setup code here, to run once:
-//  controller.write_register(RF_SETUP, 0b00000110);
   delay(1000);
   Serial.println("The delay has finished.");
-//  controller.setPALevel(RF24_PA_HIGH);
+  controller.setPALevel(RF24_PA_HIGH);
   controller.printDetails();
+//  radio_init();
 
 
   //pinMode(led, OUTPUT);
@@ -43,20 +47,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  controller->setPALevel(RF24_PA_MIN);
-  controller->printDetails();
+RF24 controller(CEpin, CSpin);
+  controller.setPALevel(RF24_PA_MIN);
+  controller.printDetails();
   delay(1000);
   
-  controller->setPALevel(RF24_PA_LOW);
-  controller->printDetails();
+  controller.setPALevel(RF24_PA_LOW);
+  controller.printDetails();
   delay(1000);
 
-  controller->setPALevel(RF24_PA_HIGH);
-  controller->printDetails();
+  controller.setPALevel(RF24_PA_HIGH);
+  controller.printDetails();
   delay(1000);
 
-  controller->setPALevel(RF24_PA_MAX);
-  controller->printDetails();
+  controller.setPALevel(RF24_PA_MAX);
+  controller.printDetails();
   delay(1000);
 }
