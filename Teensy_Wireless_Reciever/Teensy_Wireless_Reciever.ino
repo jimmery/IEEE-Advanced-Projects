f55fc297b0d1ae0e366371acf2721260163b0aa3
Arnold <awoo1016@ucla.edu>
2016-11-29 11:07:32 -0800

    added some IMU receiver stuff.

--- Teensy_Wireless_Reciever/Teensy_Wireless_Reciever.ino
+++ Teensy_Wireless_Reciever/Teensy_Wireless_Reciever.ino
@@ -1,10 +1,107 @@
 #include <RF24.h>
 #include <printf.h>
+#include <Wire.h>
 // define all the pins we are using. 
 #define LED_PIN 13
 #define RED_LED 0
 #define YEL_LED 1
 #define GRN_LED 2
+
+const int MPU_addr = 0x68;
+
+typedef enum : uint8_t{
+  GYRO_PREC_250 = 0,
+  GYRO_PREC_500,
+  GYRO_PREC_1000,
+  GYRO_PREC_2000,
+} gyro_precision_e;
+
+typedef enum : uint8_t
+{
+  ACCEL_PREC_2 = 0,
+  ACCEL_PREC_4,
+  ACCEL_PREC_8,
+  ACCEL_PREC_16
+} accel_precision_e;
+
+void setSleep(bool enable){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 1, true);
+  uint8_t power = Wire.read();
+  power = ((enable) ? (power | 0b01000000) : (power & 0b10111111));
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(power);
+  Wire.endTransmission(true);
+}
+
+void getAccelData(int16_t* ax, int16_t* ay, int16_t* az){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true);
+  *ax = Wire.read() << 8 | Wire.read();
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3D);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true);
+  *ay = Wire.read() << 8 | Wire.read();
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3F);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true);
+  *az = Wire.read() << 8 | Wire.read();
+  Wire.endTransmission(true);
+}
+
+void getGyroData(int16_t* gx, int16_t* gy, int16_t* gz){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true);
+  *gx = Wire.read() << 8 | Wire.read();
+  Wire.endTransmission(true);
+  
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x45);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true);
+  *gy = Wire.read() << 8 | Wire.read();
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x47);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 2, true);
+  *gz = Wire.read() << 8 | Wire.read();
+  Wire.endTransmission(true);
+}
+
+void setGyroPrec(gyro_precision_e prec){
+  //prec &= 0b11;
+  uint8_t test = 1;
+  prec = test;
+  prec = prec<<3;
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x1B);
+  Wire.write(prec);
+  Wire.endTransmission(true);  
+}
+
+void setAccelPrec(accel_precision_e prec){
+  prec &= 0b11;
+  prec = prec<<3;
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x1C);
+  Wire.write(prec);
+  Wire.endTransmission(true);
+}
 
 // maximum buffer size, for the purpose of 
 #define BUFFER_SIZE 56
